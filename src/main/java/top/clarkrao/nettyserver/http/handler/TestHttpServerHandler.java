41f5ad1d65ee2856bad090ded452fee7fb57e9c4
clarkrao <clarkrao25@gmail.com>
2019-12-05 22:06:50 +0800

    UPDATE: 更新Handler

--- src/main/java/top/clarkrao/nettyserver/http/handler/TestHttpServerHandler.java
+++ src/main/java/top/clarkrao/nettyserver/http/handler/TestHttpServerHandler.java
@@ -6,22 +6,43 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URI;
+import java.util.Objects;
 
 /**
  * @Author: ClarkRao
  * @Date: 2019/12/5 21:28
  * @Description:
  */
+@Slf4j
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
+        if (msg instanceof HttpRequest) {
+            HttpRequest httpRequest = (HttpRequest) msg;
 
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+            log.info("请求方法名",httpRequest.method().name());
 
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+            URI uri = new URI(httpRequest.uri());
 
-        ctx.writeAndFlush(response);
+            String text = "Hello World";
+
+            if (Objects.equals("/favicon.ico", uri.getPath())) {
+                log.info("请求favicon.ico");
+                return;
+            } else if (Objects.equals("/test", uri.getPath())){
+                text = text + " Test";
+            }
+            ByteBuf content = Unpooled.copiedBuffer(text, CharsetUtil.UTF_8);
+
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
+            ctx.writeAndFlush(response);
+        }
     }
 }
